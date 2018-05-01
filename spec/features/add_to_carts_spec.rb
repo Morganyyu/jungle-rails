6e7fed080c887fe9075a2666577fc70b500f3e7d
Morganyyu <morganyu120@hotmail.com>
2018-05-01 20:05:57 +0000

    cart item quantity updates as add button is clicked

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds item to cart", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Cart quantity updates" do
+    visit root_path
+    first('article.product').find_link('Add').click
+
+
+    expect(page).to have_css 'article.product', count: 10
+  end
+
+end
